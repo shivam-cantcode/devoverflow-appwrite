07155cc55aaa21d5d948534c58f3a54dd7128096
shivam yadav <yadav.shivam7389@gmail.com>
2026-06-15 14:09:26 +0530

    code improvement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import LandingPage from "./components/LandingPage";
+import IconCloudDemo from "./components/IconCloudDemo";
 
 export default function Home() {
   return <LandingPage />;
+  return <IconCloudDemo />;
 }
